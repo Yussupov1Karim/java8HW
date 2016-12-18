210ecf9593f53b8c77df859feeec61e06fa35a69
Petr <petr.kurapov@gmail.com>
2016-12-18 20:36:18 +0300

    Initial commit

---
+++ src/ru/karim/Person.java
@@ -0,0 +1,27 @@
+package ru.karim;
+
+
+public class Person {
+    static class Persona {
+
+        private int age;
+        private String name;
+
+        Persona(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+
+        public String toString() {
+            return name + age;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public String getName() {
+            return name;
+    }
+    }
+}
